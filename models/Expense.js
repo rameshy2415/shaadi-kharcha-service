2edd40b10093b06b70e55df16f3df2b150380b6c
rameshy2415 <rameshy2415@gmail.com>
2025-03-27 22:27:58 +0530

    Removed id for expenses

--- models/Expense.js
+++ models/Expense.js
@@ -2,10 +2,6 @@
 const mongoose = require("mongoose");
 
 const ExpenseSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
